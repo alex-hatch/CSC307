9f29252da761588df99eccdf4563e5b8d791c465
alexhatch <alexhatchh@gmail.com>
2023-01-13 14:46:40 -0800

    Updates

--- src/Table.js
+++ src/Table.js
@@ -3,32 +3,48 @@ import React from 'react'
 function Table() {
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
+        <TableHeader />
+        <TableBody />
       </table>
-    );  
-}
+    );
+} 
+
+function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>         
+          <td>{row.job}</td>
+        </tr>
+      );
+     }
+    );
+    return (
+        <tbody>
+          {rows}
+         </tbody>
+     );
+  }
+  
+  
+  function Table(props) {
+      return (
+        <table>
+          <TableHeader />
+          <TableBody characterData={props.characterData} />
+        </table>
+      );
+  }
+
+function TableHeader()  {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    );
+  }
 
 export default Table;
