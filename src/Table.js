ed1f4ebfde7fc6cc0560ed3bc1f727d5ce166172
alexhatch <alexhatchh@gmail.com>
2023-01-16 17:15:21 -0800

    Added dynamic data entry and deletion

--- src/Table.js
+++ src/Table.js
@@ -1,44 +1,41 @@
-import React from 'react'
+import React from "react";
 
 function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-      return (
-<tr key={index}>
-  <td>{row.name}</td>
-  <td>{row.job}</td>
-  <td>
-    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-  </td>
-</tr>
-      );
-     }
-    );
+  const rows = props.characterData.map((row, index) => {
     return (
-        <tbody>
-          {rows}
-         </tbody>
-     );
-  }
-  
-  
-  function Table (props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
-      </table>
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     );
-  }
+  });
+  return <tbody>{rows}</tbody>;
+}
 
-function TableHeader()  {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-    );
-  }
+function Table(props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
+    </table>
+  );
+}
+
+function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+}
 
 export default Table;
